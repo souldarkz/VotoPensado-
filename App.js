7e8f9e5e43a5beebd3d985768b55892c36261ade
Trebor87j <jhonrobert1987@gmail.com>
2020-08-20 21:55:12 -0500

    modificado y actualizado navegación tipo tab

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator, StackView } from '@react-navigation/stack';
-import {Dignidades} from "./pantallas/Dignidades"
+import { Dignidades } from "./pantallas/Dignidades"
+import { Candidatos } from "./pantallas/Candidatos"
+import { Detalle } from "./pantallas/Detalle"
 const Tab = createBottomTabNavigator();
 const HomeStack = createStackNavigator();
 
@@ -28,7 +30,9 @@ export default class App extends Component {
   render(){
     return  <NavigationContainer>
     <Tab.Navigator >
-      <Tab.Screen name="Dignidades" component={DignidadesScreen} />
+      <Tab.Screen name="Dignidades" component={Dignidades}></Tab.Screen>
+      <Tab.Screen name="Candidatos" component={Candidatos}></Tab.Screen>
+      <Tab.Screen name="Detalle" component={Detalle}></Tab.Screen>
     </Tab.Navigator>
   </NavigationContainer>
   }}
